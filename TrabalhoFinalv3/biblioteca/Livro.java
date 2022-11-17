aa440351ccfe7f3bc86a023b51bac2fa96cbea75
incursio26 <51689090+incursio26@users.noreply.github.com>
2022-11-16 22:49:16 -0300

    Add files via upload
    Padrão state para empréstimos dos livros

---
+++ TrabalhoFinalv3/biblioteca/Livro.java
@@ -0,0 +1,63 @@
+package biblioteca;
+
+import estadosLivros.State;
+
+public class Livro 
+{
+	public int codigo;
+	public String titulo;
+	public String editora;
+	public String autores;
+	public int edicao;
+	public int publicacao;
+	private State state; //devido ao padrao state
+	
+	public void setState(State state) 
+	{
+		this.state = state;
+		
+	}	
+	public void getState()
+	{
+		System.out.println(state);
+	}
+	
+	
+	//gets & sets
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getEditora() {
+		return editora;
+	}
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+	public String getAutores() {
+		return autores;
+	}
+	public void setAutores(String autores) {
+		this.autores = autores;
+	}
+	public int getEdicao() {
+		return edicao;
+	}
+	public void setEdicao(int edicao) {
+		this.edicao = edicao;
+	}
+	public int getPublicacao() {
+		return publicacao;
+	}
+	public void setPublicacao(int publicacao) {
+		this.publicacao = publicacao;
+	}
+}
---
+++ TrabalhoFinalv3/usuarios/Aluno.java
@@ -0,0 +1,11 @@
+package usuarios;
+
+import biblioteca.Usuario;
+
+public class Aluno extends Usuario
+{
+	public Aluno(int codigo, String nome) 
+	{
+		super(codigo, nome);
+	}
+}
